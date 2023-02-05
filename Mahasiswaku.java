21c2682045287d850a9664152cca013f4c8a8ac0
arsyikamarifatika <93310658+arsyikamarifatika@users.noreply.github.com>
2023-02-05 20:58:47 +0700

    Add files via upload

---
+++ Mahasiswaku.java
@@ -0,0 +1,14 @@
+ 
+package TugasSesi_9;
+public class Mahasiswaku {
+     
+    public static void main(String[] args) {
+        Mahasiswa <String,String, Integer> m = new Mahasiswa<>();
+        m. setNim("20210040063");
+        m. setName ("Arsyika Ma'rifatika Suryana Putri");
+        m. setClas(21);
+        System.out.println(m.getNim());
+        System.out.println(m.getName ());
+        System.out.println(m.getClas());
+    }
+}
